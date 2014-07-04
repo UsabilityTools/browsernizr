9faea2325b127b073f6f8d703a323eb5ed2a2c41
Eryk Napierała <eryk.piast@gmail.com>
2014-07-04 12:01:33 +0200

    allow to add tests after module initialization

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ testRunner();
 // Remove the "no-js" class if it exists
 setClasses(classes);
 
-delete ModernizrProto.addTest;
-delete ModernizrProto.addAsyncTest;
-
 // Run the things that are supposed to run after the tests
 for (var i = 0; i < Modernizr._q.length; i++) {
   Modernizr._q[i]();
